bfb4e2aa654c01e22f08484303e97942534e49f8
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2024-09-28 18:05:23 +0300

    commit

--- src/FastMeiliSync.API/Roles/Endpoints/V1/RoleModule.cs
+++ src/FastMeiliSync.API/Roles/Endpoints/V1/RoleModule.cs
@@ -9,11 +9,20 @@ public sealed class RoleModule : ICarterModule
         var groupe = app.MapGroup(Router.RoleRoutes.V1.BASE_ROLE_URL)
             .WithTags(nameof(Role).ToLower());
 
-        groupe.MapPost(Router.RoleRoutes.V1.Seed, RoleEndpoints.HandleSeedAsync);
-        groupe.MapPost(string.Empty, RoleEndpoints.HandleCreateAsync);
-        groupe.MapPut(string.Empty, RoleEndpoints.HandleUpdateAsync);
-        groupe.MapDelete(string.Empty, RoleEndpoints.HandleDeleteByIdAsync);
-        groupe.MapGet(string.Empty, RoleEndpoints.HandleGetByIdAsync);
-        groupe.MapGet(Router.RoleRoutes.V1.Paginate, RoleEndpoints.HandlePaginateAsync);
+        groupe
+            .MapPost(Router.RoleRoutes.V1.Seed, RoleEndpoints.HandleSeedAsync)
+            .RequireAuthorization();
+
+        groupe.MapPost(string.Empty, RoleEndpoints.HandleCreateAsync).RequireAuthorization();
+
+        groupe.MapPut(string.Empty, RoleEndpoints.HandleUpdateAsync).RequireAuthorization();
+
+        groupe.MapDelete(string.Empty, RoleEndpoints.HandleDeleteByIdAsync).RequireAuthorization();
+
+        groupe.MapGet(string.Empty, RoleEndpoints.HandleGetByIdAsync).RequireAuthorization();
+
+        groupe
+            .MapGet(Router.RoleRoutes.V1.Paginate, RoleEndpoints.HandlePaginateAsync)
+            .RequireAuthorization();
     }
 }
